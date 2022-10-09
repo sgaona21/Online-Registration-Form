b6536d903a241808cfd830cc2db53545423e29e7
sgaona21 <stevegaona21@gmail.com>
2022-10-08 18:15:35 -0700

    updates

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("js linked");
+
+const phoneNumber = document.getElementById("phone");
+
+phoneNumber.addEventListener("keydown", (event) => {
+    if (event.code  != "Numpad5") {
+        
+    }
+})
